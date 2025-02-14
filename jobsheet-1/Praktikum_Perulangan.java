6140fcea568a90a46bf1f1cafc83e0bca50cbc90
Fata-Haidar <fatahaidaraly@gmail.com>
2025-02-14 14:03:59 +0700

    Praktikum perulangan

--- jobsheet-1/Praktikum_Perulangan.java
+++ jobsheet-1/Praktikum_Perulangan.java
@@ -3,6 +3,25 @@ public class Praktikum_Perulangan {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Input NIM: ");
+        System.out.print("Input NIM: ");
+        String nim = sc.nextLine();
+        String nim2 = nim.substring(nim.length() - 2);
+        int angkaInt = Integer.parseInt(nim2);
+        if (angkaInt < 10) {
+            angkaInt+=10;
+            
+        }
+        for (int i = 1; i <= angkaInt; i++) {
+            if (i==6||i==10) {
+                continue;
+            }
+            if (i % 2 == 1) {
+               System.out.print("* "); 
+            }else{
+                System.out.print(i+" ");
+            }
+             
+            
+        }
     }
 }
