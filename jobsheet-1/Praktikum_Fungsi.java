a754b1ed9a0b5c0e729c42487872487b8b1f01ec
Fata-Haidar <fatahaidaraly@gmail.com>
2025-02-17 18:01:30 +0700

    fungsi

---
+++ jobsheet-1/Praktikum_Fungsi.java
@@ -0,0 +1,54 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Praktikum_Fungsi {
+   
+     static int[][] stockBunga = {{10, 5, 15, 7}, {6, 11, 9, 12}, {12, 2, 10, 10}, {5, 5, 7, 12}   
+    };
+    static int[] hargaBunga = {75000, 50000, 60000, 10000}; 
+    static String[] cabang = {"RoyalGarden 1", "RoyalGarden 2", "RoyalGarden 3", "RoyalGarden 4"};
+    static String[] jenisBunga = {"Aglonema", "Keladi", "Alocasia", "Mawar"};
+    public static void main(String[] args) {          
+                    tampilkanPendapatan();
+               
+                    
+                    tampilkanStockCabang(3);           
+                 
+                    kurangiStockKarenaMati(3);
+                   
+    }
+    static void tampilkanPendapatan() {
+        System.out.println("\n==== Pendapatan Setiap Cabang ====");
+        for (int i = 0; i < stockBunga.length; i++) {
+            int pendapatan = 0;
+            for (int j = 0; j < stockBunga.length; j++) {
+                pendapatan += stockBunga[i][j] * hargaBunga[j];
+            }
+            System.out.println(cabang[i] + " : Rp " + pendapatan);
+        }
+    }
+    static void tampilkanStockCabang(int index) { 
+        System.out.println("\n==== Stok Bunga di " + cabang[index] + " ====");
+        for (int i = 0; i < stockBunga.length; i++) {
+                System.out.println(jenisBunga[i] + " : " + stockBunga[index][i]);      
+            
+        }
+    
+    }
+    static void kurangiStockKarenaMati(int index) {
+        int[] bungaMati = {1, 2, 0, 5};  
+        for (int i = 0; i < stockBunga.length; i++) {
+            for (int j = 0; j < stockBunga[i].length; j++) {
+                stockBunga[i][j] -= bungaMati[j];  
+            }
+        }
+        System.out.println("\n==== Stok Bunga di " + cabang[index] + " ====");
+        for (int i = 0; i < stockBunga.length; i++) {
+                System.out.println(jenisBunga[i] + " : " + stockBunga[index][i]);  
+    }
+    System.out.println("Pengurangan stock akibat bunga yang mati");
+} 
+}
+   
+
