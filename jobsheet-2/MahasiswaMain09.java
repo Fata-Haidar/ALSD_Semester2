091e76bd630ad7b5e32529ad1fe85b830b405b8c
Fata-Haidar <fatahaidaraly@gmail.com>
2025-02-21 13:38:44 +0700

    Percobaan1

---
+++ jobsheet-2/MahasiswaMain09.java
@@ -0,0 +1,13 @@
+public class MahasiswaMain09 {
+
+    public static void main(String[] args) {
+    Mahasiswa09 mhs1 = new Mahasiswa09();
+    mhs1.nama = "Fata";
+    mhs1.nim = "244107020108";
+    mhs1.kelas = "TI 1H";
+    mhs1.ipk = 3.55;
+    mhs1.tampilkanInformasi();
+    mhs1.ubahKelas("TI 1K");
+    mhs1.updateIPK(3.60);
+    mhs1.tampilkanInformasi();
+    }
